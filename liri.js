8b1c3272a20fcabf87a670a0d7ce9a1134c07d12
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-05-18 11:34:26 -0500

    adding spotify

--- liri.js
+++ liri.js
@@ -3,24 +3,39 @@ require('dotenv').config();
 var keysFile = require('./keys.js');
 var request = require("request");
 
-//var spotify = new Spotify(keysFile.spotify);
-//var Spotify= require('./node-spotify-api')
+
+
+var spotify = require('spotify');
+//var Twitter = require('twitter');
+
+function getMyTweets() {
+    var client = new Twitter(keysFile.twitter);
+    var myID = '996501445997486081';
+    var params = { id: myID, count: 20 };
+    client.get('statuses/user_timeline/', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var myTweets = 'Username: ' + tweets[i].user.name + '||' + ' Latest Tweet Date & Time: ' + tweets[i].created_at + '||' + ' Tweet: ' + tweets[i].text
+                console.log(myTweets);
+                //console.log(tweets);
+            }
 
 
 
-var Twitter = require('twitter');
-var client = new Twitter(keysFile.twitter);
-var myID='996501445997486081';
-var params = 
-{
-    id: myID,
-    count:20,
-    text:""
-};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-      console.log(tweets);
-  }
+        }
+    });
+}
+
+
+
+
+ 
+spotify.search({ type: 'artist', query: 'nsync' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    console.log(data);
+ 
+    // Do something with 'data'
 });
-
-
