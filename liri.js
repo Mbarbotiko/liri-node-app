2ad4a49a2ca613cdc51a2d58a6c8d6b3597f676c
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-05-18 15:01:27 -0500

    IMDB added, command line intake added, spotify still doesnt work

--- liri.js
+++ liri.js
@@ -4,9 +4,30 @@ var keysFile = require('./keys.js');
 var request = require("request");
 
 
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var liriCommands = process.argv[2];
 
-var spotify = require('spotify');
-//var Twitter = require('twitter');
+// for(var i=2;i<=liriCommands;i++){
+//figure out code to accept muliple word movies
+// }
+
+//do-what-it-says
+
+switch (liriCommands) {
+    case "spotify-this-song":
+        getSpotify()
+        break;
+    case "my-tweets":
+       getMyTweets();
+        break;
+    case "movie-this":
+    getMovies();
+        break;
+   default:
+        console.log("Try entering one of these commands. 1. spotify-this-song, 2. my-tweets, 3. movie-this ")
+}
+
 
 function getMyTweets() {
     var client = new Twitter(keysFile.twitter);
@@ -28,14 +49,44 @@ function getMyTweets() {
 
 
 
+function getSpotify() {
+    var spotify = new Spotify(keysFile.spotify);
+    spotify.search({ type: 'track', query: 'barbie girl' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
- 
-spotify.search({ type: 'artist', query: 'nsync' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
+        console.log(data);
+        console.log("Track: " + data.tracks.items);
+
+        //get artist name, track name, preview link of the song , the album its from
+    });
+
+}
+
+function getMovies() {
+    var movieName = process.argv[3];
+    if (!movieName) {
+        movieName = "mr nobody"
     }
-    console.log(data);
- 
-    // Do something with 'data'
-});
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+        var fix = JSON.parse(body)
+
+        if (!error && response.statusCode === 200) {
+
+            console.log("Release Year: " + fix.Year + "||",
+                "Title: " + fix.Title + "||",
+                "Rated: " + fix.Rated + "||",
+                "Rotten Tomatoes Rating: " + fix.Ratings[1].Value + "||",
+                "Language: " + fix.Language + "||",
+                "Produced In: " + fix.Country + "||",
+                "Plot: " + fix.Plot + "||",
+                "Actors: " + fix.Actors);
+        }
+    });
+
+}
+
